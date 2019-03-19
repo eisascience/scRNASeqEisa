a8f675b0a8cbf4e4c19cf4352f7eb2f7f21710b0
EM <em@EMs-MacBook-Pro.ohsu.edu>
2019-03-19 13:33:41 -0700

    init

--- R/CustomFXs_SourceAll_local.R
+++ R/CustomFXs_SourceAll_local.R
@@ -1,11 +1,1 @@
 
-
-##### this scripts loads all the custom fxs
-Path.to.SF = "/Volumes/Maggie/Work/OHSU/Eisa/R/scRNASeq/rbin"
-SourceFiles <- list.files(Path.to.SF, pattern = ".R", full.names = T)[!grepl("SourceAll", list.files(Path.to.SF, 
-                                                                                                     pattern = ".R"))]
-
-for(SF in SourceFiles){
-  print(SF)
-  source(SF)
-}
